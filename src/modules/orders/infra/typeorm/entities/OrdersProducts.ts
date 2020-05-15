26a8c5fdbc4ef9f1a026b4b051cb9b5b9e483060
Everton Almeida <everton.o.almeida@outlook.com>
2020-05-15 15:51:19 +0100

    Added Typeorm Data Mapper and migrations

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -11,23 +11,35 @@ import {
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
 
+@Entity('ordersProducts')
 class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Order, order => order.order_products, { eager: true })
+  @JoinColumn({ name: 'order_id' })
   order: Order;
 
-  product: Product;
-
-  product_id: string;
-
+  @Column()
   order_id: string;
 
+  @ManyToOne(() => Product, product => product.order_products, { eager: true })
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column()
+  product_id: string;
+
+  @Column('float')
   price: number;
 
+  @Column('float')
   quantity: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -9,19 +9,27 @@ import {
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('products')
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column('float')
   price: number;
 
+  @Column('decimal', { precision: 5, scale: 2 })
   quantity: number;
 
+  @OneToMany(() => OrdersProducts, ordersProducts => ordersProducts.order)
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
